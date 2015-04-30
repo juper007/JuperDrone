4b65c6de904ae5d0234e2e0b517a665a63be4dbc
Min Park <juper007@gmail.com>
2015-04-30 13:43:22 -0700

    Change index to for syntex

--- helloworld.py
+++ helloworld.py
@@ -7,32 +7,29 @@ import numpy
 import os
 
 def getAngleGyro(gyro, gyro_dxyz, dt):
-	gyro[0] = gyro_dxyz[0] * dt
-	gyro[1] = gyro_dxyz[1] * dt
-	gyro[2] = gyro_dxyz[2] * dt
-	return gyro
+    for i in range(3):
+        gyro[i] = gyro_dxyz[i] * dt 
+    return gyro
 
 def getAngleAcc(acc):
-	g = 9.80665
-	pi = 3.141592
-	result = [0,0,0]
+    g = 9.80665
+    pi = 3.141592
+    result = [0,0,0]
 
-	result[0] = math.atan2(acc[1], acc[2] + g) * 180 / pi
-	result[1] = math.atan2(acc[0], acc[2] + g) * 180 / pi
-	result[2] = math.atan2(acc[0], acc[1] + g) * 180 / pi
-	return result
+    result[0] = math.atan2(acc[1], acc[2] + g) * 180 / pi
+    result[1] = math.atan2(acc[0], acc[2] + g) * 180 / pi
+    result[2] = math.atan2(acc[0], acc[1] + g) * 180 / pi
+    return result
 
 def getAngleCombine(gyro, acc, prev_angle, dt):
-	tau = 0.003
-	result = [0,0,0]
-	
-	a = tau / (tau + dt)
-	result[0] = a * (acc[0] + gyro[0] * dt) + (1 - a) * prev_angle[0]
-	result[1] = a * (acc[1] + gyro[1] * dt) + (1 - a) * prev_angle[1]
-	a=1 # Ignore Accel sensor for Z axle
-	result[2] = a * (acc[2] + gyro[2] * dt) + (1 - a) * prev_angle[2]
+    tau = 0.003
+    result = [0,0,0]
+    
+    a = tau / (tau + dt)
+    for i in range(3):
+        result[i] = a * (acc[i] + gyro[i] * dt) + (1 - a) * prev_angle[i]   
 
-	return result
+    return result
 
 s = L3GD20(busId = 1, slaveAddr = 0x6b, ifLog = False, ifWriteBlock=False)
 lsm = Adafruit_LSM303()
@@ -62,16 +59,16 @@ kf = KalmanFilterLinear(A,B,H,xhat,P,Q,R)
 s.Init()
 s.Calibrate()
 
-while True:	
-	gyro_dxyz = s.Get_CalOut_Value()
-	gyro = getAngleGyro(gyro, gyro_dxyz, dt)
-	acc = getAngleAcc(lsm.read())
-	acc = kf.filter(numpy.matrix([0, 0, 0]), acc)
-	current_angle = getAngleCombine(gyro, acc, current_angle, dt)
+while True: 
+    gyro_dxyz = s.Get_CalOut_Value()
+    gyro = getAngleGyro(gyro, gyro_dxyz, dt)
+    acc = getAngleAcc(lsm.read())
+    acc = kf.filter(numpy.matrix([0, 0, 0]), acc)
+    current_angle = getAngleCombine(gyro, acc, current_angle, dt)
 
-	os.system('clear')
+    os.system('clear')
 
-	print("{:7.2f} {:7.2f} {:7.2f}".format(gyro[0], gyro[1], gyro[2]))
-	print("{:7.2f} {:7.2f} {:7.2f}".format(acc[0], acc[1], acc[2]))
-	print("{:7.2f} {:7.2f} {:7.2f}".format(current_angle[0], current_angle[1], current_angle[2]))
-	time.sleep(dt)
+    print("{:7.2f} {:7.2f} {:7.2f}".format(gyro[0], gyro[1], gyro[2]))
+    print("{:7.2f} {:7.2f} {:7.2f}".format(acc[0], acc[1], acc[2]))
+    print("{:7.2f} {:7.2f} {:7.2f}".format(current_angle[0], current_angle[1], current_angle[2]))
+    time.sleep(dt)
