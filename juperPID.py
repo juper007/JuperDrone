aff572d87e911504aebaf6bbafaa910bf5d09112
Min Park <juper007@gmail.com>
2015-05-14 17:09:45 -0700

    Add target_angle var

--- juperPID.py
+++ juperPID.py
@@ -4,11 +4,12 @@ class juperPID(object):
     def __init__(self):
         self.x_PID = pid()
         self.y_PID = pid()
+        self.target_angle = [0,0]
 
     def getPID(self, senserStatus, timeStep):
         result = [0,0,0,0]
-        x_Correction = self.x_PID.calc(target_angle[0], sensorStatus[0], timeStep)
-        y_Correction = self.y_PID.calc(target_angle[1], sensorStatus[1], timeStep)
+        x_Correction = self.x_PID.calc(target_angle[0], senserStatus[0], timeStep)
+        y_Correction = self.y_PID.calc(target_angle[1], senserStatus[1], timeStep)
 
         result[0] = x_Correction / 2
         result[1] = x_Correction / 2 * -1
