a7a3c9298f9b8fc447abfdeb36c25340a48faa41
Min Park <juper007@gmail.com>
2015-05-22 14:53:19 -0700

    Add DataLog function

--- juperDrone.py
+++ juperDrone.py
@@ -9,6 +9,16 @@ import thread
 def log(message):
     print ("[Main] {0} : {1}".format(datetime.datetime.now(), message))
 
+def dataLog():
+    f = open('DataLog.csv', 'a')
+    data = '{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}\n'.format(datetime.datetime.now(),
+        sensorStatus[0],sensorStatus[1],sensorStatus[2],
+        pid.x_corr, pid.y_corr,
+        motorStatus[0], motorStatus[1], motorStatus[2], motorStatus[3]
+        )
+
+    f.write(data)
+
 def doBluetoothCommand(comm):
     if (comm.hasCommand):
         command = comm.command.pop(0)
@@ -46,11 +56,12 @@ def doBluetoothCommand(comm):
 
 if __name__ == "__main__":
     log ("Start")
-
     # init values
     timeStep = 0.1
     motors_pin_array = [17, 18, 22, 23]
     motors_correction = [0, 0, 0, 0]
+    motorStatus = [0, 0, 0, 0]
+    sensorStatus = [0, 0, 0]    
 
     # init sensor
     sensors = juperSensor()
@@ -65,6 +76,8 @@ if __name__ == "__main__":
     
     while bluetooth.isRunning:
         if (bluetooth.isConnected):
+            doBluetoothCommand(bluetooth)
+            
             # Get sensor value
             sensorStatus = sensors.getCurrentAngleStatus()
 
@@ -75,11 +88,13 @@ if __name__ == "__main__":
             for i in range(4):
                 #i=i
                 motors.setMotorW(i, motors_correction[i])
+
+            motorStatus = motors.getCurrentStatus()
            
-            bluetooth.sendMotorStatus(motors.getCurrentStatus())
+            bluetooth.sendMotorStatus(motorStatus)
             bluetooth.sendSensorStatus(sensorStatus)
 
-            doBluetoothCommand(bluetooth)
+        dataLog()
         time.sleep(timeStep)
 
     # Check motor if Drone is still flying
--- juperPID.py
+++ juperPID.py
@@ -5,6 +5,8 @@ class juperPID(object):
     def __init__(self):
         self.x_PID = pid(0.070, 0.025, 0.010, 15)
         self.y_PID = pid(0.070, 0.025, 0.010, 15)
+        self.x_corr = 0
+        self.y_corr = 0
         self.target_angle = [0,0]
 
     def log(self, message):
@@ -12,11 +14,11 @@ class juperPID(object):
 
     def getPID(self, senserStatus, timeStep):
         result = [0,0,0,0]
-        x_corr = self.x_PID.calc(self.target_angle[0], senserStatus[0], timeStep)
-        y_corr = self.y_PID.calc(self.target_angle[1], senserStatus[1], timeStep)
+        self.x_corr = self.x_PID.calc(self.target_angle[0], senserStatus[0], timeStep)
+        self.y_corr = self.y_PID.calc(self.target_angle[1], senserStatus[1], timeStep)
 
-        result[0] = (x_corr / 2) + -(y_corr / 2)
-        result[1] = (x_corr / 2) + (y_corr / 2)
-        result[2] = -(x_corr / 2) + -(y_corr / 2)
-        result[3] = -(x_corr / 2) + (y_corr / 2)
+        result[0] = (self.x_corr / 2) + -(self.y_corr / 2)
+        result[1] = (self.x_corr / 2) + (self.y_corr / 2)
+        result[2] = -(self.x_corr / 2) + -(self.y_corr / 2)
+        result[3] = -(self.x_corr / 2) + (self.y_corr / 2)
         return result
